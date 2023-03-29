fa7eba7f519e108d76f10048e07fa1e282e1ad36
greg-aires <hiepdt2909@gmail.com>
2023-03-30 03:33:37 +0700

    JWT

--- helpers/connect_Redis.js
+++ helpers/connect_Redis.js
@@ -1,7 +1,7 @@
 const redis = require('redis')
 
 const client = redis.createClient({
-  password: 'Fb3LtniUAptuNHKJp1IralG5B48rp7DN',
+  password: process.env.PASS_REDIS,
   socket: {
     host: 'redis-19938.c295.ap-southeast-1-1.ec2.cloud.redislabs.com',
     port: 19938,
@@ -11,7 +11,7 @@ const client = redis.createClient({
 client.on('error', (err) => console.log('Redis Client Error', err))
 
 client.connect()
-client.on('connect',() => console.log('Redis Client OK', ))
+client.on('connect',() => console.log('Redis Client OK' ))
 
 // client.disconnect()
 
